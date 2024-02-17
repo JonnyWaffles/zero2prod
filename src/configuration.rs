eb0cb28dcc7d714ff1af6a2f4d88aadf58977053
Jonathan Fuller <mr.waffles@gmail.com>
2024-02-17 13:42:48 -0500

    More digital Ocean stuff

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,8 @@
 //! src/configuration.rs
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::ConnectOptions;
+use sqlx::postgres::{PgConnectOptions,PgSslMode};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -9,6 +12,7 @@ pub struct Settings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -17,31 +21,33 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{username}:{password}@{host}:{port}/{database_name}",
-            username = self.username,
-            password = self.password.expose_secret(),
-            host = self.host,
-            port = self.port,
-            database_name = self.database_name,
-        ))
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{username}:{password}@{host}:{port}",
-            username = self.username,
-            password = self.password.expose_secret(),
-            host = self.host,
-            port = self.port,
-        ))
+    pub fn with_db(&self) -> PgConnectOptions {
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing_log::log::LevelFilter::Trace);
+        self.without_db().database(&self.database_name)
     }
 }
 
@@ -66,6 +72,14 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(config::File::from(
             configuration_directory.join(environment_filename),
         ))
+        // Add in settings from environment variables (with a prefix of APP and
+        // '__' as separator)
+        // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__")
+        )
         .build()?;
 
     settings.try_deserialize::<Settings>()
