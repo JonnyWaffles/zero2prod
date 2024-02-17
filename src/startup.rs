f16d2f93a0f436cbf0e7fa15e7aa953ddfe091c9
Jonathan Fuller <mr.waffles@gmail.com>
2024-02-17 09:09:48 -0500

    Finished Chapter 4

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the pool using web::Data, which boils down to an Arc smart pointer
@@ -9,6 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Use the move keyword to capture connection from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(crate::routes::health_check))
             .route("/subscriptions", web::post().to(crate::routes::subscribe))
             .app_data(db_pool.clone())
