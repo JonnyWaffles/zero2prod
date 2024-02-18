ec467c5e837cbf84abb6215bde19761e28dcf8e7
Jonathan Fuller <mr.waffles@gmail.com>
2024-02-18 13:06:30 -0500

    Fixed clippy

--- src/configuration.rs
+++ src/configuration.rs
@@ -39,13 +39,13 @@ impl DatabaseSettings {
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
-            .password(&self.password.expose_secret())
+            .password(self.password.expose_secret())
             .port(self.port)
             .ssl_mode(ssl_mode)
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        let mut options = self.without_db().database(&self.database_name);
+        let options = self.without_db().database(&self.database_name);
         options.log_statements(tracing_log::log::LevelFilter::Trace);
         self.without_db().database(&self.database_name)
     }
